1929a5dfc2166fcc668beb0e2d91fa5c2ceb2d61
longpapa82-cyber <longpapa82@gmail.com>
2026-03-22 16:31:12 +0900

    chore: bump versionCode to 32

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -40,7 +40,7 @@ export default ({ config }) => ({
     },
     edgeToEdgeEnabled: true,
     package: 'com.longpapa82.travelplanner',
-    versionCode: config.android?.versionCode ?? 31,
+    versionCode: config.android?.versionCode ?? 32,
     intentFilters: [
       {
         action: 'VIEW',
